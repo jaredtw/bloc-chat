56e406ae4ca374163fcf4d32d9c19f3e32a887d3
jaredtw <jaredtimothywillis@gmail.com>
2017-11-11 20:11:43 -0800

    checkpoint 4

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,27 @@
 (function() {
-    function HomeCtrl($scope, Room) {
+    function HomeCtrl($scope, Room, Message) {
         $scope.chatRooms = Room.all;
+        $scope.currentRoom = null;
+        $scope.messages = null;
+
+        $scope.open = function () {
+            $uibModal.open({
+                templateUrl: 'roomsModal.html',
+                controller: 'ModalInstanceCtrl'
+            })
+        };
+
+        $scope.setCurrentRoom = function (room) {
+            $scope.currentRoom = room
+            console.log("Current Room ID: " + $scope.currentRoom.$id);
+
+            $scope.messages = Message.getByRoomId(room.$id);
+        };
+
+        return $scope.chatRooms;
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['$scope', 'Room', HomeCtrl]);
+        .controller('HomeCtrl', ['$scope', 'Room', 'Message', HomeCtrl]);
 })();
