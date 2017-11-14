9c58b4ec3a061e4603bf0d3c5915e9bb5893e018
jaredtw <jaredtimothywillis@gmail.com>
2017-11-14 00:40:46 -0800

    checkpoint 5 progress

---
+++ app/scripts/controllers/UserModalInstanceCtrl.js
@@ -0,0 +1,16 @@
+(function () {
+    	function UserModalInstanceCtrl($scope, $uibModalInstance, $cookies) {
+        		this.ok = function () {
+        			$cookies.put('blocChatCurrentUser', this.name);
+
+        			console.log("You just created a new user: " + this.name);
+        			console.log("You just created a new cookie: " + $cookies.blocChatCurrentUser);
+
+        			$uibModalInstance.close(this.name);
+        		};
+    	}
+
+  	angular
+  		.module('blocChat')
+  		.controller('UserModalInstanceCtrl', ['$scope', '$uibModalInstance', '$cookies', UserModalInstanceCtrl]);
+})();
